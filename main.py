4802128b4678f945c7c1ad5dd311d08a034ba5ab
AlizhaDwiPutriYana <104014352+AlizhaDwiPutriYana@users.noreply.github.com>
2022-04-21 22:12:45 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="alizha" : print("Kamu Lucuu abieeez!!!!")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="alizha":
+# 	print("kamu Lucuu abieeeez!")
+# 	print("kamu juga keren banget!")
+# print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama == "putri":
+    print("hai putri, si cantik!!!")
+else:
+    print("Ah kamu bukan putri, kamu gak cantik! :(")
+
+print("akhir dari program")
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
